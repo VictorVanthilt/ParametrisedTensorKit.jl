dc2803e3915d09d136c4cb555288330fafd58de3
Victor Vanthilt <victor.vanthilt@gmail.com>
2023-12-05 23:33:16 +0100

    Add storagetype (works) and block (doesnt work)

--- tests/PTMTest.jl
+++ tests/PTMTest.jl
@@ -28,15 +28,17 @@ H_Ising = MPOHamiltonian(data);
 # 0  0  Z
 # 0  0  1
 
+
 T = ComplexF64
-Z = TensorMap(T[0 1; 1 0], ℂ^2 ← ℂ^2)
+Z = TensorMap(T[1 0; 0 -1], ℂ^2 ← ℂ^2)
 hX = sinSx
 
 data = Array{Any,3}(missing, 1, 3, 3)
-data[1, 1, 1] = identity(ℂ^2)
+
+data[1, 1, 1] = one(T) # regular numbers are interpreted as identity operators
 data[1, 1, 2] = -Z
-data[1, 1, 3] = hX
 data[1, 2, 3] = Z
-data[1, 3, 3] = identity(ℂ^2)
+data[1, 1, 3] = sinSx
+data[1, 3, 3] = one(T)
 
 H_Ising = MPOHamiltonian(data);
