840f439374a4b79b543dbe598d89d8a02eba809b
Victor <victor.vanthilt@gmail.com>
2023-12-06 22:12:05 +0100

    Lukas code

--- src/parametrisedtensormap.jl
+++ src/parametrisedtensormap.jl
@@ -13,11 +13,67 @@ TensorKit.domain(t::ParametrisedTensorMap) = domain(t.tensor)
 TensorKit.codomain(t::ParametrisedTensorMap) = codomain(t.tensor)
 
 function (PTM::ParametrisedTensorMap)(t::Number)
-    return PTM.coeff(t) * PTM.tensor
+    # return PTM.coeff(t) * PTM.tensor
+    # alternative:
+    return ParametrisedTensorMap(PTM.tensor, PTM.coeff(t))
 end
 
 TensorKit.storagetype(::Type{<:ParametrisedTensorMap{S,N1,N2,T}}) where {S,N1,N2,T} = TensorKit.storagetype(T)
 
-TensorKit.block(t::ParametrisedTensorMap{S,N1,N2,T}, ::Trivial) where {S,N1,N2,T} = t.tensor.data
+TensorKit.block(t::ParametrisedTensorMap{S,N1,N2,T,E}, ::Trivial) where {S,N1,N2,T, E<:Number} = TensorKit.block(t.tensor, Trivial()) * t.coeff
 
+function Base.:*(t1::ParametrisedTensorMap, t2::ParametrisedTensorMap)
+    newtens = t1.tensor * t2.tensor
+    return ParametrisedTensorMap(newtens, combinecoeff(t1.coeff, t2.coeff))
+    # return mul!(similar(t1, promote_type(scalartype(t1), scalartype(t2)), codomain(t1) ← domain(t2)), t1.tensor, t2.tensor)
+end
 
+function Base.:*(t1::ParametrisedTensorMap, t2::AbstractTensorMap)
+    newtens = t1.tensor * t2
+    return ParametrisedTensorMap(newtens, combinecoeff(t1.coeff, one(promote_type(scalartype(t1), scalartype(t2)))))
+    # return mul!(similar(t1, promote_type(scalartype(t1), scalartype(t2)), codomain(t1) ← domain(t2)), t1.tensor, t2)
+end
+
+function Base.:*(t1::AbstractTensorMap, t2::ParametrisedTensorMap)
+    newtens = t1 * t2.tensor
+    return ParametrisedTensorMap(newtens, combinecoeff(one(promote_type(scalartype(t1), scalartype(t2))), t2.coeff))
+    # return mul!(similar(t1, promote_type(scalartype(t1), scalartype(t2)), codomain(t1) ← domain(t2)), t1, t2.tensor)
+end
+
+function combinecoeff(f1::Function, f2::Number)
+    return (t) -> f1(t) * f2
+end
+
+function combinecoeff(f1::Number, f2::Function)
+    return (t) -> f1 * f2(t)
+end
+
+function combinecoeff(f1::Function, f2::Function)
+    return (t) -> f1(t) * f2(t)
+end
+
+function combinecoeff(f1::Number, f2::Number)
+    return f1 * f2
+end
+
+function MPSKit.ismpoidentity(::ParametrisedTensorMap)
+    return false
+end
+
+function eval_coeff(t::ParametrisedTensorMap, tval)
+    return t(tval)
+end
+eval_coeff(t::Any, tval) = t
+
+function (H::MPOHamiltonian{T})(t) where {S,T<:BlockTensorMap{S,2,2,<:Union{MPSKit.MPOTensor, ParametrisedTensorMap, TensorKit.BraidingTensor}}}
+    return MPOHamiltonian(map(H.data) do x
+        new_subtensors = Dict(I => eval_coeff(old_subtensor, t) for (I, old_subtensor) in nonzero_pairs(x))
+        new_tensortype = Union{(typeof.(values(new_subtensors)))...}
+
+        newx = BlockTensorMap{S,2,2,new_tensortype}(undef, x.codom, x.dom)
+        for (key, value) in new_subtensors
+            newx[key] = value
+        end
+        return newx
+    end)
+end
--- tests/PTMTest.jl
+++ tests/PTMTest.jl
@@ -1,27 +1,10 @@
-using BlockTensorKit, MPSKit, TensorKit, MPSKitModels
-include("../src/parametrisedtensormap.jl")
+using Revise, BlockTensorKit, MPSKit, TensorKit, MPSKitModels
+includet("../src/parametrisedtensormap.jl")
 
 # Sinusoidal coefficient
 coeff(t) = sin(t*2π)
+typeof(coeff)
 sinSx = ParametrisedTensorMap(S_x(), coeff)
-
-# Jordan block mpo form
-# 1 C D
-# 0 A B
-# 0 0 1
-
-T = ComplexF64
-X = TensorMap(T[0 1; 1 0], ℂ^2 ← ℂ^2)
-Z = TensorMap(T[1 0; 0 -1], ℂ^2 ← ℂ^2)
-
-data = Array{Any,3}(missing, 1, 3, 3)
-data[1, 1, 1] = identity(ℂ^2)
-data[1, 1, 1] = one(T) # regular numbers are interpreted as identity operators
-data[1, 1, 2] = -Z
-data[1, 2, 3] = Z
-data[1, 1, 3] = 3 * X
-H_Ising = MPOHamiltonian(data);
-
 
 # Ising Hamiltonian mpo
 # 1 -Z h(t)X
@@ -35,10 +18,14 @@ hX = sinSx
 
 data = Array{Any,3}(missing, 1, 3, 3)
 
-data[1, 1, 1] = one(T) # regular numbers are interpreted as identity operators
-data[1, 1, 2] = -Z
-data[1, 2, 3] = Z
-data[1, 1, 3] = hX
-data[1, 3, 3] = one(T)
+data[1, 1, 1] = one(T); # regular numbers are interpreted as identity operators
+data[1, 1, 2] = -Z;
+data[1, 2, 3] = Z;
+data[1, 1, 3] = hX;
+data[1, 3, 3] = one(T);
 
-H_Ising = MPOHamiltonian(data);
+H_Ising = MPOHamiltonian(data)
+
+O_Ising = make_time_mpo(H_ising, dt, TaylorCluster)
+
+timestep
