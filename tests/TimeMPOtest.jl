bf869a547b48541cd246364e246001be1ee4b7d0
Victor Vanthilt <victor.vanthilt@gmail.com>
2024-02-03 23:43:52 +0100

    timeevompo's are still far away

--- tests/TimeMPOtest.jl
+++ tests/TimeMPOtest.jl
@@ -9,11 +9,11 @@ f(t) = sin(t)
 
 data = Array{Any,3}(missing, 1, 3, 3)
 
-data[1, 1, 1] = one(T); # regular numbers are interpreted as identity operators
+data[1, 1, 1] = 1; # regular numbers are interpreted as identity operators
 data[1, 1, 2] = -S_z();
 data[1, 2, 3] = f*S_x();
 data[1, 1, 3] = f*S_x();
-data[1, 3, 3] = one(T);
+data[1, 3, 3] = 1;
 
 H = MPOHamiltonian(data)
 
