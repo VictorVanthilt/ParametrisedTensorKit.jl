c38a3d0f3ca31b38a434251da953feb9f6017c5f
Victor Vanthilt <victor.vanthilt@gmail.com>
2024-02-03 23:09:48 +0100

    Stop including coefficient type in PTM struct

--- src/parametrisedtensormap.jl
+++ src/parametrisedtensormap.jl
@@ -4,16 +4,16 @@ import TensorOperations.tensorcontract!
 import LinearAlgebra.mul!
 import LinearAlgebra.lmul!
 
-struct ParametrisedTensorMap{S,N1,N2,T<:AbstractTensorMap{S,N1,N2},E} <: AbstractTensorMap{S,N1,N2}
+struct ParametrisedTensorMap{S,N1,N2,T<:AbstractTensorMap{S,N1,N2}} <: AbstractTensorMap{S,N1,N2}
     tensor::T
-    coeff::E
+    coeff
 end
 
 const PTM = ParametrisedTensorMap
 const TIMEDOP = Union{PTM, SumOfTensors}
 
-function ParametrisedTensorMap(tensor::T, coeff::E) where {S,N1,N2,T<:AbstractTensorMap{S,N1,N2},E}
-    return ParametrisedTensorMap{S,N1,N2,T,E}(tensor, coeff)
+function ParametrisedTensorMap(tensor::T, coeff) where {S,N1,N2,T<:AbstractTensorMap{S,N1,N2}}
+    return ParametrisedTensorMap{S,N1,N2,T}(tensor, coeff)
 end
 
 function ParametrisedTensorMap(tensor::T) where {T<:AbstractTensorMap}
