ae7f035ae782441e1e91d623a4547640c9b386aa
Victor Vanthilt <victor.vanthilt@gmail.com>
2024-02-03 23:29:55 +0100

    add new test cases

--- tests/MPOHamTest.jl
+++ tests/MPOHamTest.jl
@@ -3,15 +3,33 @@ includet("../src/sumoftensors.jl")
 includet("../src/parametrisedtensormap.jl")
 includet("../src/mpstools.jl")
 
-f = t -> sin(t)
+function f(t)
+    return sin(t)
+end
+
+function g(t)
+    return cos(t)
+end
+
 fσx = f * S_x()
+fσz = f * S_z()
 
-Lattice = FiniteChain(5)
+gσx = g * S_x()
+gσz = g * S_z()
 
-H = @mpoham fσx{Lattice[1]} + fσx{Lattice[2]} + fσx{Lattice[1]}*fσx{Lattice[2]}
+sot1 = fσx + fσz
+sot2 = gσx + gσz
+
+Lattice = FiniteChain(2)
+
+H = @mpoham fσx{Lattice[1]} + gσz{Lattice[2]}
 
 H(1)
 
-H₋ = @mpoham fσx{Lattice[1]} - fσx{Lattice[2]} 
+H_sum = @mpoham sot1{Lattice[1]} + sot2{Lattice[2]}
 
-H₋(1)
+H_sum(1)
+
+H_mixed = @mpoham fσx{Lattice[1]} + sot1{Lattice[2]}
+
+eltype((sot2, sot1))
