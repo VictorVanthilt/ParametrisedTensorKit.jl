2e680e2e4ced604482f840bbee5d6b22be81d67b
victorvanthilt <victor.vanthilt@gmail.com>
2024-03-10 12:14:21 +0100

    simpler time_evo_mpo formation

--- tests/TimeMPOtest.jl
+++ tests/TimeMPOtest.jl
@@ -1,19 +1,15 @@
 using Revise, BlockTensorKit, MPSKit, TensorKit, MPSKitModels
 
-includet("../src/ParametrisedTensorKit.jl")
+includet("../src/newParametrisedTensorKit.jl")
 using .ParametrisedTensorKit
 
 f(t) = sin(t)
+g(t) = cos(t)
 
-data = Array{Any,3}(missing, 1, 3, 3)
+N = 1
+Lat = FiniteChain(N)
 
-data[1, 1, 1] = 1; # regular numbers are interpreted as identity operators
-data[1, 1, 2] = -S_z();
-data[1, 2, 3] = f*S_x();
-data[1, 1, 3] = f*S_x();
-data[1, 3, 3] = 1;
-
-H = MPOHamiltonian(data)
+H = @mpoham (f*σᶻ()){Lat[1]} + (g*σˣ()){Lat[1]}
 
 time_mpo = make_time_mpo(H, 0.1, WI)
 
