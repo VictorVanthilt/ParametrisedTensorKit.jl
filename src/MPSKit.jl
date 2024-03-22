742a834401619dc5b6328a606241e33e1807b17a
victorvanthilt <victor.vanthilt@gmail.com>
2024-03-22 10:58:32 +0100

    change BTM signature

--- src/MPSKit.jl
+++ src/MPSKit.jl
@@ -16,7 +16,7 @@ const TIMEDOP = ParametrisedTensorMap
 function (H::MPOHamiltonian{T})(t) where {S,E<:Number,T<:BlockTensorMap{E,S,2,2}}
     return MPOHamiltonian(map(H.data) do x
         new_subtensors = Dict(I => old_subtensor isa TIMEDOP ? eval_coeff(old_subtensor, t) : old_subtensor for (I, old_subtensor) in nonzero_pairs(x))
-        newx = BlockTensorMap{S,2,2,E}(undef, x.codom, x.dom)
+        newx = BlockTensorMap{E,S,2,2}(undef, x.codom, x.dom)
         for (key, value) in new_subtensors
             newx[key] = value
         end
