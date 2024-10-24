5da1a0f180b51ff268dd15ab744df6eb0997b8f6
victor <victor.vanthilt@gmail.com>
2024-10-24 10:18:01 +0200

    update delay functionality for new MPSKit datastructures

--- src/MPSKit.jl
+++ src/MPSKit.jl
@@ -12,26 +12,14 @@ function (O::MPO{T})(t) where {TT, T<:SparseBlockTensorMap{TT}}
     end)
 end
 
-# MPSKit.ismpoidentity(::ParametrisedTensorMap) = false
-
-function delay(H::MPOHamiltonian{T}, dt::Number) where {E<:Number,S,T<:BlockTensorMap{E,S,N₁,N₂}} where {N₁,N₂}
-    return MPOHamiltonian(map(H.data) do x
-        new_subtensors = Dict(I => old_subtensor isa PTM ? delay(old_subtensor, dt) : old_subtensor for (I, old_subtensor) in nonzero_pairs(x))
-        newx = BlockTensorMap{E,S,N₁,N₂}(undef, x.codom, x.dom)
-        for (key, value) in new_subtensors
-            newx[key] = value
-        end
-        return newx
+function delay(H::MPOHamiltonian{T}, dt::Number) where {TT, T<:SparseBlockTensorMap{TT}}
+    return MPOHamiltonian(map(parent(H)) do x
+        return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? delay(X, dt) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
     end)
 end
 
-function delay(H::SparseMPO{T}, dt::Number) where {E<:Number,S,T<:BlockTensorMap{E,S,N₁,N₂}} where {N₁,N₂}
-    return InfiniteMPO(map(H.data) do x
-        new_subtensors = Dict(I => old_subtensor isa PTM ? delay(old_subtensor, dt) : old_subtensor for (I, old_subtensor) in nonzero_pairs(x))
-        newx = BlockTensorMap{E,S,N₁,N₂}(undef, x.codom, x.dom)
-        for (key, value) in new_subtensors
-            newx[key] = value
-        end
-        return newx
+function delay(O::MPO{T}, dt) where {TT, T<:SparseBlockTensorMap{TT}}
+    return MPO(map(parent(O)) do x
+        return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? delay(X, dt) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
     end)
 end
