ca0967837cae6070ff8f04ebed0b06e709550ed1
victor <victor.vanthilt@gmail.com>
2024-10-24 10:25:58 +0200

    reduce code duplication with  macro

--- src/MPSKit.jl
+++ src/MPSKit.jl
@@ -1,25 +1,15 @@
 const PTM = ParametrisedTensorMap
 
-function (H::MPOHamiltonian{T})(t) where {TT, T<:SparseBlockTensorMap{TT}}
-    return MPOHamiltonian(map(parent(H)) do x
-        return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? X(t) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
-    end)
-end
+for type in [MPO, MPOHamiltonian]
+    @eval function delay(H::$type{T}, dt::Number) where {TT, T<:SparseBlockTensorMap{TT}}
+            return $type(map(parent(H)) do x
+                return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? delay(X, dt) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
+            end)
+        end
 
-function (O::MPO{T})(t) where {TT, T<:SparseBlockTensorMap{TT}}
-    return MPO(map(parent(O)) do x
-        return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? X(t) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
-    end)
-end
-
-function delay(H::MPOHamiltonian{T}, dt::Number) where {TT, T<:SparseBlockTensorMap{TT}}
-    return MPOHamiltonian(map(parent(H)) do x
-        return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? delay(X, dt) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
-    end)
-end
-
-function delay(O::MPO{T}, dt) where {TT, T<:SparseBlockTensorMap{TT}}
-    return MPO(map(parent(O)) do x
-        return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? delay(X, dt) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
-    end)
+    @eval function (O::$type{T})(t) where {TT, T<:SparseBlockTensorMap{TT}}
+            return $type(map(parent(O)) do x
+                return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? X(t) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
+            end)
+        end
 end
