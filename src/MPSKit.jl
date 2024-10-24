fa7999ab0460b852164feb23ce25f6cffa0c235f
victor <victor.vanthilt@gmail.com>
2024-10-24 10:30:43 +0200

    formatting

--- src/MPSKit.jl
+++ src/MPSKit.jl
@@ -1,15 +1,15 @@
 const PTM = ParametrisedTensorMap
 
 for type in [MPO, MPOHamiltonian]
-    @eval function delay(H::$type{T}, dt::Number) where {TT, T<:SparseBlockTensorMap{TT}}
-            return $type(map(parent(H)) do x
-                return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? delay(X, dt) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
-            end)
-        end
+    @eval function delay(H::$type{T}, dt::Number) where {TT,T<:SparseBlockTensorMap{TT}}
+        return $type(map(parent(H)) do x
+            return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? delay(X, dt) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
+        end)
+    end
 
-    @eval function (O::$type{T})(t) where {TT, T<:SparseBlockTensorMap{TT}}
-            return $type(map(parent(O)) do x
-                return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? X(t) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
-            end)
-        end
+    @eval function (O::$type{T})(t) where {TT,T<:SparseBlockTensorMap{TT}}
+        return $type(map(parent(O)) do x
+            return SparseBlockTensorMap{TT}(Dict(I => X isa PTM ? X(t) : X for (I, X) in nonzero_pairs(x)), codomain(x), domain(x))
+        end)
+    end
 end
