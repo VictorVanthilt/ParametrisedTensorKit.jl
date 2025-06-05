82a64c60569263decdc767653955f8bfe08d5add
victor <victor.vanthilt@gmail.com>
2025-06-05 14:27:50 +0200

    simplify TensorKit method implementations

--- src/TensorKit.jl
+++ src/TensorKit.jl
@@ -78,30 +78,10 @@ function TK.:⊗(t1::ParametrisedTensorMap, t2::ParametrisedTensorMap)
     return sum(ptms)
 end
 
-function TK.removeunit(t::ParametrisedTensorMap, i::Int; kwargs...)
-    return ParametrisedTensorMap(
-        map(t.tensors) do t
-            TK.removeunit(t, i; kwargs...)
-        end,
-        deepcopy(t.coeffs)
-    )
-end
+TK.removeunit(t::ParametrisedTensorMap, i::Int; kwargs...) = ParametrisedTensorMap(TK.removeunit.(t.tensors, i; kwargs...), deepcopy(t.coeffs))
 
-function TK.insertleftunit(t::ParametrisedTensorMap, i::Int; kwargs...)
-    return ParametrisedTensorMap(
-        map(t.tensors) do t
-            TK.insertleftunit(t, i; kwargs...)
-        end,
-        deepcopy(t.coeffs)
-    )
-end
+TK.insertleftunit(t::ParametrisedTensorMap, i::Int; kwargs...) = ParametrisedTensorMap(TK.insertleftunit.(t.tensors, i; kwargs...), deepcopy(t.coeffs))
 
-function TK.insertrightunit(t::ParametrisedTensorMap, i::Int; kwargs...)
-    return ParametrisedTensorMap(
-        map(t.tensors) do t
-            TK.insertrightunit(t, i; kwargs...)
-        end,
-        deepcopy(t.coeffs)
-    )
-end
+TK.insertrightunit(t::ParametrisedTensorMap, i::Int; kwargs...) = ParametrisedTensorMap(TK.insertrightunit.(t.tensors, i; kwargs...), deepcopy(t.coeffs))
+
 # TK.storagetype(t::AbstractTensorMap) = Matrix{scalartype(t)}
